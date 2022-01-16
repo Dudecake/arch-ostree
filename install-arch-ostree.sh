e074ba852587b1abfde6cc45dddbe750ee697528
Dudecake <coen_koomen@hotmail.com>
2022-01-16 12:51:22 +0100

    Fixed setting up boot config

--- install-arch-ostree.sh
+++ install-arch-ostree.sh
@@ -28,10 +28,12 @@ if [[ $NEW_ROOT -ne 0 ]]; then
 fi
 cp -an "${1}/ostree/deploy/arch/deploy/${hash}.0/usr/lib/ostree-boot/efi/." "${1}/boot/efi"
 cp -an "${1}/ostree/deploy/arch/deploy/${hash}.0/usr/lib/ostree-boot/grub/." "${1}/boot/grub"
-kver=$(ls -1 "${1}/ostree/deploy/arch/deploy/${hash}.0/usr/lib/modules")
-boot_hash=$(find "${1}/boot/ostree" -type f -name vmlinuz-${kver} | grep -Po '(?<=arch-)[a-f0-9]+')
+kver="$(ls -1 "${1}/ostree/deploy/arch/deploy/${hash}.0/usr/lib/modules")"
+boot_hash="$(find "${1}/boot/ostree" -type f -name vmlinuz-${kver} | grep -Po '(?<=arch-)[a-f0-9]+')"
+boot_config="${1}/boot/loader/entries/$(ls -1 "${1}/boot/loader/entries" | head -n1)"
 cp "${1}/ostree/deploy/arch/deploy/${hash}.0/usr/lib/ostree-boot/initramfs-linux.img" "${1}/boot/ostree/arch-${boot_hash}/initramfs-${kver}.img"
-echo "initrd /ostree/${boot_hash}/initramfs-${kver}.img" >> "$(ls -1 "${1}/boot/loader/entries" | head -n1)"
+sed -i "s:ostree=.*:& root=UUID=$(basename ${1}) scsi_mod.use_blk_mq=1 zswap.enabled=1 zswap.compressor=lz4 zswap.zpool=z3fold rd.timeout=15:" ${boot_config}
+echo "initrd /ostree/arch-${boot_hash}/initramfs-${kver}.img" >> "${boot_config}"
 
 # mkdir "${1}/sysroot"
 # rm -rf "${1}/home" "${1}/root"
