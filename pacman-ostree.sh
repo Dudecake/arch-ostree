ebf6e1017bbf3fbad5b6b723acdd67ddba07eb20
Dudecake <coen_koomen@hotmail.com>
2022-03-12 13:42:53 +0100

    Fixed `pacman-ostree` script

--- pacman-ostree.sh
+++ pacman-ostree.sh
@@ -13,14 +13,12 @@ fi
 set -e
 ostree pull arch-ostree
 ostree upgrade
-# ref=$()
 boot_file="$(grep -L initrd /boot/loader/entries/ostree-1-arch.conf /boot/loader/entries/ostree-2-arch.conf)"
 if [[ ! -z ${boot_file} ]]; then
   boot_hash="$(grep -Po '[0-9a-f]{64}' ${boot_file} | head -n1)"
-  hash="$(basename $(readlink /ostree/boot.0/arch/${boot_hash}/0))"
+  hash="$(basename $(readlink /ostree/boot.*/arch/${boot_hash}/0))"
   kver=$(ls -1 "/ostree/deploy/arch/deploy/${hash}/usr/lib/modules")
-  hash=${hash:0:64}
-  [[ ! -f "/boot/ostree/${os}-${some_ref}/initramfs-${kver}.img" ]] && cp "/ostree/deploy/arch/deploy/${hash}/usr/lib/ostree-boot/initramfs-linux.img" "/boot/ostree/arch-${boot_hash}/initramfs-${kver}.img"
+  [[ ! -f "/boot/ostree/${os}-${hash:0:64}/initramfs-${kver}.img" ]] && cp "/ostree/deploy/arch/deploy/${hash}/usr/lib/ostree-boot/initramfs-linux.img" "/boot/ostree/arch-${boot_hash}/initramfs-${kver}.img"
   echo "initrd /ostree/arch-${boot_hash}/initramfs-${kver}.img" >> ${boot_file}
 fi
 update-grub
